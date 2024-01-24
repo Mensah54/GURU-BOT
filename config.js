ff416d265dddb54ba69962f840387c00b0958b4f
Mensah54 <150304946+Mensah54@users.noreply.github.com>
2024-01-24 15:35:55 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['233246995732', 'URAN', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
